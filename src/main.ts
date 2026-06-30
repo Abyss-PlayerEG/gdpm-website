71b60d2ba13f7f03c2becd0593574dbc2fb33ebf
PlayerEG <enderg@qq.com>
2026-07-01 00:23:05 +0800

    refactor: remove DevTools detector, clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { i18n } from './i18n'
 import { banner } from './utils/banner'
-import { useDevtoolsDetector } from './composables/useDevtoolsDetector'
 import './style.css'
 
 console.log(
@@ -14,11 +13,5 @@ console.log(
   '%c🚀 Interested in contributing? Check out our GitHub repo!',
   'color: #48BB78; font-weight: bold; font-size: 13px;'
 )
-
-// DevTools detector (production only)
-if (!import.meta.env.DEV) {
-  const { startDetection } = useDevtoolsDetector()
-  startDetection()
-}
 
 createApp(App).use(router).use(i18n).mount('#app')
